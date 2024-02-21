78df08e30f46cdb242f9fbba4142275217943180
Jose Gaston <jmcbgaston@gmail.com>
2024-02-21 15:33:36 -0600

    add UI feedback for requests

--- public/app.js
+++ public/app.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const submitButton = document.querySelector(".post-req > button");
   const updateButton = document.querySelector(".patch-req > button");
   const deleteButton = document.querySelector(".delete-req > button");
-  const externalApiButton = document.querySelector(".external-api > button");
+  // const externalApiButton = document.querySelector(".external-api > button");
+
+  const getMessage = document.querySelector(".get-req > .message");
+  const submitMessage = document.querySelector(".post-req > .message");
+  const updateMessage = document.querySelector(".patch-req > .message");
+  const deleteMessage = document.querySelector(".delete-req > .message");
+  // const externalApiMessage = document.querySelector(".external-api > .message");
   // *** *** *** ***
 
   // *** EVENT HANDLERS ***
@@ -12,43 +18,63 @@ document.addEventListener("DOMContentLoaded", () => {
   submitButton.addEventListener("click", () => postText());
   updateButton.addEventListener("click", () => patchText());
   deleteButton.addEventListener("click", () => deleteText());
-  externalApiButton.addEventListener("click", () => callFakeApi());
+  // externalApiButton.addEventListener("click", () => callFakeApi());
   // *** *** *** ***
+
+  function displayTimedMessage(element, message, reqStatus) {
+    reqStatus === "success"
+      ? (element.style["color"] = "green")
+      : (element.style["color"] = "red");
+
+    element.style["visibility"] = "visible";
+    element.textContent = message;
+    setTimeout(() => {
+      element.style["visibility"] = "hidden";
+      element.textContent = "";
+    }, 5000);
+  }
 
   // *** API ***
   async function getText() {
     const spanText = document.querySelector(".get-req > p > span");
     try {
       const response = await axios.get("/get-text");
-      console.log({ axiosGET: response.data });
 
       spanText.textContent = response.data.storedText;
+
+      displayTimedMessage(getMessage, "Data successfully fetched", "success");
     } catch (error) {
-      console.log({ error });
+      displayTimedMessage(getMessage, error, "error");
     }
   }
 
   async function postText() {
     const inputValue = document.querySelector(".post-req > input").value;
     try {
-      const response = await axios.post("/post-text", { inputValue });
-      console.log({ axiosPOST: response.data });
+      await axios.post("/post-text", { inputValue });
 
       storedText = inputValue;
+
+      displayTimedMessage(submitMessage, "Data successfully posted", "success");
     } catch (error) {
-      console.log({ error });
+      displayTimedMessage(submitMessage, error, "error");
     }
   }
 
   async function patchText() {
     const inputValue = document.querySelector(".patch-req > input").value;
     try {
-      const response = await axios.patch("/patch-text", { inputValue });
-      console.log({ axiosPATCH: response.data });
+      await axios.patch("/patch-text", { inputValue });
 
       storedText = inputValue;
+
+      displayTimedMessage(
+        updateMessage,
+        "Data successfully patched",
+        "success"
+      );
     } catch (error) {
-      console.log({ error });
+      displayTimedMessage(updateMessage, error, "error");
     }
   }
 
@@ -56,19 +82,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const spanText = document.querySelector(".get-req > p > span");
     try {
       const response = await axios.delete("/delete-text");
-      console.log({ axiosDELETE: response.data });
 
       spanText.textContent = response.data.storedText;
+
+      displayTimedMessage(
+        deleteMessage,
+        "Data successfully deleted",
+        "success"
+      );
     } catch (error) {
-      console.log({ error });
+      displayTimedMessage(deleteMessage, error, "error");
     }
   }
 
-  async function callFakeApi() {
-    await fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  }
+  // async function callFakeApi() {
+  //   await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  //     .then((response) => response.json())
+  //     .then((json) => console.log(json));
+  // }
 
   // async function callFakeApi() {
   //   let url1 = "https://pokeapi.co/api/v2/pokemon?limit=150";
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // This is where the routes are defined and how they are handled
 // TODO: A step further to take would be to attatch a database but we'll use a fake db for now ln:12
 const express = require("express");
+const favicon = require("serve-favicon");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// *** FAVICON ***
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// *** *** *** ***
+
 // *** SERVE FILES FROM:  ***
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 // *** *** *** ***
 
 // *** BODY PARSER ***
@@ -25,47 +31,51 @@ app.get("/", async (req, res) => {
 
 app.get("/get-text", async (req, res) => {
   try {
-    storedText ||= "________";
-    console.log({ expressGET: storedText });
-
-    res.send({ storedText });
+    if (storedText) {
+      res.send({ storedText });
+    } else {
+      throw error;
+    }
   } catch (error) {
-    console.log({ error });
+    res.status(500).send("External server error");
   }
 });
 
 app.post("/post-text", async (req, res) => {
   try {
-    storedText = req.body.inputValue;
-    console.log({ expressPOST: { storedText } });
-
-    res.send(`Input received: ${storedText}`);
+    if (req.body.inputValue) {
+      storedText = req.body.inputValue;
+      res.send(`Input received: ${storedText}`);
+    } else {
+      throw error;
+    }
   } catch (error) {
-    console.log({ error });
     res.status(500).send("External server error");
   }
 });
 
 app.patch("/patch-text", async (req, res) => {
   try {
-    storedText = req.body.inputValue;
-    console.log({ expressPATCH: { storedText } });
-
-    res.send(`Input received: ${storedText}`);
+    if (req.body.inputValue) {
+      storedText = req.body.inputValue;
+      res.send(`Input received: ${storedText}`);
+    } else {
+      throw error;
+    }
   } catch (error) {
-    console.log({ error });
     res.status(500).send("External server error");
   }
 });
 
 app.delete("/delete-text", async (req, res) => {
   try {
-    storedText = "________";
-    console.log({ expressDELETE: { storedText } });
-
-    res.send("Stored text deleted");
+    if (storedText) {
+      storedText = null;
+      res.send({ storedText });
+    } else {
+      throw error;
+    }
   } catch (error) {
-    console.log({ error });
     res.status(500).send("External server error");
   }
 });
